947f3988301ee34782260d6859a3d6318bf11aca
dermie83 <dermiemadsen@gmail.com>
2024-06-05 20:19:43 +0100

    components completed

---
+++ vite-project/src/types/interfaces.ts
@@ -0,0 +1,10 @@
+export interface ModuleProps { 
+    name: string, 
+    noLectures: number, 
+    noPracticals: number 
+}
+
+export interface ProgrammeProps { 
+    title: string, 
+    modules: ModuleProps[] 
+}
